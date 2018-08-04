814daa95290ec8fdf513d3f188604bc4a496f973
Preeti <pwadhwani2592@gmail.com>
2018-08-04 18:51:33 +0530

    Added app.json file

---
+++ NewsApp/App.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import Button from './components/Button';
+
+// Update the message and reload the app to see the changes
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<Props> {
+
+  state = {
+    count: 0
+  };
+
+  //must define render method
+  render() {
+      //return JSX
+      //should always return single child
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+          <Text style={styles.instructions}>{`Count: ${this.state.count}`}</Text>
+        <Button onPress={this.updateCount}/>
+      </View>
+    );
+  }
+
+  updateCount = () =>{
+    this.setState({count: this.state.count+1});
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
